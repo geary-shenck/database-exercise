c37538bf579c99a24dc1c8f8b7646aef0fa7086c
Geary Shenck <geary.shenck@gmail.com>
2022-07-01 14:34:18 -0500

    exercsises 1-10 for tables in the sql tab

---
+++ table_exercises.sql
@@ -0,0 +1,19 @@
+-- codeup exercise for tables
+use employees;
+
+show tables;
+
+show create table employees;
+
+describe employees;
+
+-- Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
+-- 	emp_no
+-- Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
+-- 	first_name, last_name, gender
+-- Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
+-- 	birth_date, hire_date
+-- What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
+-- 	departments is within the employees database
+
+show create table dept_manager;
