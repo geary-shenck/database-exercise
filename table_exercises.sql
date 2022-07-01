82c94bd1515c36e55f6dcda85dff8592a3d4ab00
Geary Shenck <geary.shenck@gmail.com>
2022-07-01 14:48:09 -0500

    added more info for exercises

--- table_exercises.sql
+++ table_exercises.sql
@@ -6,14 +6,21 @@ show tables;
 show create table employees;
 
 describe employees;
+describe departments;
+DESCRIBE dept_emp;
+describe dept_manager;
+describe employees;
+describe salaries;
+describe titles;
 
 -- Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
--- 	emp_no
+-- 	salaries, departments, employees, dept_emp, dept_manager, titles
 -- Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
--- 	first_name, last_name, gender
+-- 	departments, dept_manager, employees, titles, dept_emp, titles
 -- Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
--- 	birth_date, hire_date
+-- 	dept_emp, employees, dept_manager, salaries, titles
 -- What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
 -- 	departments is within the employees database
-
+-- Show the SQL that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
+-- show create table dept_manager;
 show create table dept_manager;
