647f8011abe0b867233ad070afe55750d982b1d7
Geary Shenck <geary.shenck@gmail.com>
2022-07-05 10:57:57 -0500

    reformatted for easier reading in relaiton to the ask

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,59 +1,54 @@
 -- for exercise in the basic-statement 
+
+-- Explore the structure of the albums table.
 -- use albums_db database
 USE albums_db;
-/* Explore the structure of the albums table.
-a. How many rows are in the albums table?
-b. How many unique artist names are in the albums table?
-c. What is the primary key for the albums table?
-d. What is the oldest release date for any album in the albums table? What is the most recent release date? */
-
 SHOW tables;
 
--- a 
+-- a. How many rows are in the albums table?
 SELECT * FROM albums_db.albums;
 -- 31
-d
--- b
+
+-- b. How many unique artist names are in the albums table?
 SELECT DISTINCT artist FROM albums_db.albums;
 -- 23
 
--- c
+-- c. What is the primary key for the albums table?
 DESCRIBE albums;
 -- id
 
--- d
+-- d. What is the oldest release date for any album in the albums table? 
+-- What is the most recent release date?
 SELECT release_date from albums;
 -- manual sort 1967, 2011
 
-/*Write queries to find the following information:
-a. The name of all albums by Pink Floyd
-b. The year Sgt. Pepper's Lonely Hearts Club Band was released
-c. The genre for the album Nevermind
-d. Which albums were released in the 1990s
-e. Which albums had less than 20 million certified sales
-f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"? */
+
+
+-- Write queries to find the following information:
+
 SELECT * FROM albums_db.albums;
--- a
+-- a. The name of all albums by Pink Floyd
 SELECT name FROM albums_db.albums WHERE artist = 'Pink Floyd';
 -- the dark side of the moon, the wall
 
--- b
+-- b. The year Sgt. Pepper's Lonely Hearts Club Band was released
 SELECT release_date FROM albums_db.albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 -- 1967
 
--- c
+-- c. The genre for the album Nevermind
 SELECT genre FROM albums_db.albums WHERE name = 'Nevermind';
 -- Grunge, Alternative Rock
 
--- d 
+-- d. Which albums were released in the 1990s
 SELECT name FROM albums_db.albums WHERE release_date between 1990 and 1999;
 -- 11 of them
 
--- e
+-- e. Which albums had less than 20 million certified sales
 SELECT name FROM albums_db.albums WHERE sales < 20;
 -- 13 of them
 
--- f
+/* f. All the albums with a genre of "Rock". Why do these query results not 
+include albums with a genre of "Hard rock" or "Progressive rock"? */
 SELECT name, genre FROM albums_db.albums WHERE genre = 'Rock';
 -- 5 of them, explict search for 'Rock', not inclusive
 
